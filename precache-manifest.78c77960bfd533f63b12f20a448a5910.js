cc49052363bec32901bdbde05e763051ae488be2
Javier <javier.rb_96@hotmail.com>
2021-03-07 19:21:59 +0100

    Updates

---
+++ precache-manifest.78c77960bfd533f63b12f20a448a5910.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "085c593fa1c140f31da7847d5e660712",
+    "url": "/keeper-react/index.html"
+  },
+  {
+    "revision": "aefc788d18ced7574e4e",
+    "url": "/keeper-react/static/js/2.144adea6.chunk.js"
+  },
+  {
+    "revision": "a193ae828225b8126570",
+    "url": "/keeper-react/static/js/main.2101344e.chunk.js"
+  },
+  {
+    "revision": "d5de7c58e61e06cc4dee",
+    "url": "/keeper-react/static/js/runtime-main.79fff29f.js"
+  }
+]);
